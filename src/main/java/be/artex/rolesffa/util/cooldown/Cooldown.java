ebdddb4189f55e75a811d312df45e8edd44a0ac5
Artex <a.stockelynck12@outlook.com>
2024-07-25 10:57:26 +0200

    Changed Cooldown methods

--- src/main/java/be/artex/rolesffa/util/cooldown/Cooldown.java
+++ src/main/java/be/artex/rolesffa/util/cooldown/Cooldown.java
@@ -4,32 +4,41 @@ import be.artex.rolesffa.Main;
 import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Cooldown {
-    private static String id;
-    private static ArrayList<UUID> cooldowns;
+    public final String id;
+    private final ArrayList<UUID> playerCooldowns;
+    private static final HashMap<String, Cooldown> cooldowns = new HashMap<>();
 
-    public Cooldown(String id, ArrayList<UUID> cooldowns) {
+    private Cooldown(String id) {
         this.id = id;
-        this.cooldowns = cooldowns;
+        this.playerCooldowns = new ArrayList<>();
     }
 
-    public static Cooldown create(String id) {
-        return new Cooldown(id, new ArrayList<>());
+    public static Cooldown get(String id) {
+        if (cooldowns.get(id) == null)
+            return new Cooldown(id);
+
+        return cooldowns.get(id);
     }
 
-    public void addPlayer(UUID uuid, CooldownType type) {
-        cooldowns.add(uuid);
+    public void addPlayer(UUID uuid, long time, Cooldown cooldown) {
+        if (playerCooldowns.contains(uuid))
+            return;
 
-        Bukkit.getScheduler().runTaskLater(Main.instance, () -> cooldowns.remove(uuid), type.getTime());
+        cooldowns.put(cooldown.id, cooldown);
+
+        playerCooldowns.add(uuid);
+
+        Bukkit.getScheduler().runTaskLater(Main.instance, () -> {
+            playerCooldowns.remove(uuid);
+            cooldowns.put(cooldown.id, cooldown);
+        }, time);
     }
 
     public boolean isPlayerInCooldown(UUID uuid) {
-        return cooldowns.contains(uuid);
-    }
-
-    public static String getId() {
-        return id;
+        return playerCooldowns.contains(uuid);
     }
 }
