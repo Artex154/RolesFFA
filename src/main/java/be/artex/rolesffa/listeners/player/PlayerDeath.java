c22a84b2e3707f192f5cfe04f9de29f550a0801f
Artex <a.stockelynck12@outlook.com>
2024-08-02 19:56:05 +0200

    Stuff now doesn't drop

--- src/main/java/be/artex/rolesffa/listeners/player/PlayerDeath.java
+++ src/main/java/be/artex/rolesffa/listeners/player/PlayerDeath.java
@@ -7,10 +7,12 @@ import be.artex.rolesffa.util.StringUtils;
 import be.artex.rolesffa.util.api.RoleUtils;
 import be.artex.rolesffa.util.cooldown.Cooldown;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerDeath implements Listener {
     @EventHandler
@@ -21,6 +23,8 @@ public class PlayerDeath implements Listener {
         Role role = RoleUtils.getPlayerRole(player.getUniqueId());
 
         Cooldown.removePlayerFromAllCooldowns(player.getUniqueId());
+
+        event.getDrops().clear();
 
         if (role == null) {
             Main.instance.getLogger().warning(player.getUniqueId().toString() + " (" + player.getName() + ") died with no role");
@@ -33,6 +37,11 @@ public class PlayerDeath implements Listener {
             event.setDeathMessage(StringUtils.line + "\n" + ChatColor.GREEN + player.getName() + ChatColor.GRAY + " est mort.\nSon rôle était: " + role.getName() + ChatColor.GRAY + ".\n" + StringUtils.line);
 
             return;
+        }
+
+        for (ItemStack itemStack : event.getDrops()) {
+            if (itemStack.getType().equals(Material.GOLDEN_APPLE))
+                killer.getInventory().addItem(new ItemStack(Material.GOLDEN_APPLE));
         }
 
         event.setDeathMessage(StringUtils.line + "\n" + ChatColor.GREEN + player.getName() + ChatColor.GRAY + " a été assassiné par " + ChatColor.RED + killer.getName() + ChatColor.GRAY + ".\nSon rôle était: " + role.getName() + ChatColor.GRAY + ".\n" + StringUtils.line);
