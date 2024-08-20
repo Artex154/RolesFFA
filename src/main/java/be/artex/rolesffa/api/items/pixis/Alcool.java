9234e47c71671a35f5d50715e47f18fc251d6837
Artex <a.stockelynck12@outlook.com>
2024-08-20 14:56:44 +0200

    Added Alcool item

--- src/main/java/be/artex/rolesffa/api/items/pixis/Alcool.java
+++ src/main/java/be/artex/rolesffa/api/items/pixis/Alcool.java
@@ -1,10 +1,8 @@
-package be.artex.rolesffa.api.items.luffy;
+package be.artex.rolesffa.api.items.pixis;
 
-import be.artex.rolesffa.Main;
 import be.artex.rolesffa.api.SPItem;
 import be.artex.rolesffa.util.cooldown.Cooldown;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -14,23 +12,23 @@ import org.bukkit.potion.PotionEffectType;
 
 import java.util.UUID;
 
-public class Gear4 extends SPItem {
+public class Alcool extends SPItem {
 
-    // todo: Create ItemStack;
+    // todo: create an ItemStack;
     @Override
     public ItemStack getItemStack() {
-        return new ItemStack(Material.NETHER_STAR);
+        return new ItemStack(Material.GLASS_BOTTLE);
     }
 
-    // todo: Create a Hover
+    // todo: create a description for a hover;
     @Override
     public TextComponent getDescription() {
-        return new TextComponent("a");
+        return null;
     }
 
     @Override
     public void onClick(PlayerInteractEvent event) {
-        Cooldown cooldown = Cooldown.get("gear_4");
+        Cooldown cooldown = Cooldown.get("alcool");
         Player player = event.getPlayer();
         UUID uuid = player.getUniqueId();
 
@@ -39,15 +37,9 @@ public class Gear4 extends SPItem {
             return;
         }
 
-        player.setMaxHealth(player.getMaxHealth() + 4);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 10, 0));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 10, 0));
 
-        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 600, 0));
-
-        Bukkit.getScheduler().runTaskLater(Main.instance, () -> {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 400, 1));
-            player.setMaxHealth(player.getMaxHealth() - 4);
-        }, 400);
-
-        cooldown.addPlayer(uuid, 80*20);
+        cooldown.addPlayer(uuid, 45*20L);
     }
 }
