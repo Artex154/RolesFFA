904bf6e1ec77683e2824e46536aa04e2e72f4516
Artex154 <a.stockelynck12@outlook.com>
2025-07-09 23:32:49 +0200

    Added game rules

--- src/main/java/be/artex/rolesffa/Main.java
+++ src/main/java/be/artex/rolesffa/Main.java
@@ -4,19 +4,27 @@ import be.artex.rolesffa.helper.WorldHelper;
 import be.artex.rolesffa.listener.entity.player.PlayerJoin;
 import be.artex.rolesffa.listener.world.ChunkLoad;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public static Plugin instance;
+    public static World world;
 
     @Override
     public void onEnable() {
         instance = this;
+        world = Bukkit.getWorlds().getFirst();
 
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new ChunkLoad(), this);
 
-        WorldHelper.initializeWorld(Bukkit.getWorlds().getFirst());
+        WorldHelper.initializeWorld(world);
+
+        world.setGameRuleValue("naturalRegeneration", "false");
+        world.setGameRuleValue("doDaylightCycle", "false");
+        world.setGameRuleValue("doMobSpawning", "false");
+
     }
 }
