8512ebeaf445038f61cf0d22d3cdda0c4a14400f
Artex154 <a.stockelynck12@outlook.com>
2025-07-13 18:20:59 +0200

    Forgot a file

--- src/main/java/be/artex/rolesffa/stats/Resistance.java
+++ src/main/java/be/artex/rolesffa/stats/Resistance.java
@@ -5,17 +5,23 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 
 public class Resistance {
-    private static final HashMap<Player, Float> PLAYER_RESISTANCE = new HashMap<>();
+    private static final HashMap<Player, Integer> PLAYER_RESISTANCE = new HashMap<>();
 
     public static void setPlayerResistance(Player player, float resistance) {
-        PLAYER_RESISTANCE.put(player, resistance);
+        resistance = 0.2f * (resistance / 100f);
+
+        int percentage = Math.round((resistance / 0.2f) * 100);
+
+        PLAYER_RESISTANCE.put(player, percentage);
+
+        player.setWalkSpeed(resistance);
     }
 
-    public static Float getPlayerResistance(Player player) {
-        return PLAYER_RESISTANCE.getOrDefault(player, 100f);
+    public static int getPlayerResistance(Player player) {
+        return PLAYER_RESISTANCE.getOrDefault(player, 100);
     }
 
     public static void resetPlayerResistance(Player player) {
-        PLAYER_RESISTANCE.put(player, 100f);
+        PLAYER_RESISTANCE.put(player, 100);
     }
 }
